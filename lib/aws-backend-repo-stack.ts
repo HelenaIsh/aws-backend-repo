40963572a9c1341d294fe2720c18d72acba0c324
elena_ishmatova <elena_ishmatova@epam.com>
2026-01-28 17:32:31 +0100

    feat: add lambda for get products

--- lib/aws-backend-repo-stack.ts
+++ lib/aws-backend-repo-stack.ts
@@ -1,9 +1,53 @@
 import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class AwsBackendRepoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const getProductsListLambda = new lambda.Function(
+      this,
+      "GetProductsListFunction",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        memorySize: 1024,
+        timeout: cdk.Duration.seconds(5),
+        handler: "getProductsList.main",
+        code: lambda.Code.fromAsset(path.join(__dirname, "./")),
+      },
+    );
+
+    const api = new apigateway.RestApi(this, "ProductsApi", {
+      restApiName: "Products Service API",
+      description: "API for Products Service",
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+        allowHeaders: ["Content-Type", "Authorization"],
+      },
+    });
+
+    const productsFromLambdaIntegration = new apigateway.LambdaIntegration(
+      getProductsListLambda,
+      {
+        proxy: true,
+      },
+    );
+
+    const productsResource = api.root.addResource("products");
+    productsResource.addMethod("GET", productsFromLambdaIntegration);
+
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url,
+      description: "API Gateway URL",
+    });
+
+    new cdk.CfnOutput(this, "ProductsEndpoint", {
+      value: `${api.url}products`,
+      description: "Products endpoint URL",
+    });
   }
 }
---
+++ lib/getProductsList.ts
@@ -0,0 +1,26 @@
+import { products } from "./products/products";
+
+export async function main() {
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
+}
